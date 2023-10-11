6f0ddd7c739eaab6c9709f8ec21dc2cc72bfc78e
shifeng <a245241964@gmail.com>
2023-10-11 16:41:15 -0400

    added CRUD operations

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
+posts = {
+    '1': {'user_id': '1', 'post': 'Hello, world!'},
+    '2': {'user_id': '2', 'post': 'My first blog post'}
+}
+
+
+@app.route('/')
+def index():
+    return 'post server up and running'
 
 @app.route('/post/<id>')
 def post(id):
-    posts = {
-        '1': {'user_id': '1', 'post': 'Hello, world!'},
-        '2': {'user_id': '2', 'post': 'My first blog post'}
-    }
+
     post_info = posts.get(id, {})
     
     # Get user info from User Service
@@ -20,6 +26,27 @@ def post(id):
             post_info['user'] = response.json()
 
     return jsonify(post_info)
+# create post
+@app.route('/create_post/<post_id>/<user_id>/<post>')
+def create_post(post_id, user_id, post):
+    posts[post_id] = {'user_id': user_id, 'post': post}
+    return jsonify(posts)
+
+# update post
+@app.route('/update_post/<id>/<user_id>/<post>')
+def update_post(id, user_id, post):
+    posts[id] = {'user_id': user_id, 'post': post}
+    return jsonify(posts)
+
+# delete post
+@app.route('/delete_post/<id>')
+def delete_post(id):
+    if id in posts:
+        del posts[id]
+        return jsonify(posts)
+    else:
+        return 'post not found'
+
 
 if __name__ == '__main__':
     app.run()
